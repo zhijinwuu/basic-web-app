7bd676f1980d30e1ef8209375632a5623a896101
zhijinwuu <zhijinw@andrew.cmu.edu>
2023-09-15 14:34:35 -0400

    add function of andrewid

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -7,5 +7,11 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  if (query.toLowerCase().includes("andrewid")) {
+    return (
+      "zhijinw"
+    );
+  }
+
   return "";
 }
