7f4c168d6111528f67826b2183d5a8f251960072
zhijinwuu <zhijinw@andrew.cmu.edu>
2023-09-19 16:18:39 -0400

    implement add and nultiply

--- utils/QueryProcessor.tsx
+++ utils/QueryProcessor.tsx
@@ -13,5 +13,19 @@ export default function QueryProcessor(query: string): string {
     );
   }
 
+  const addMatch = query.match(/What is (\d+) plus (\d+)/);
+  if (addMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
+
+  const mulMatch = query.match(/What is (\d+) multiplied by (\d+)/);
+  if (mulMatch) {
+    const x: number = parseInt(addMatch[1]);
+    const y: number = parseInt(addMatch[2]);
+    return (x+y).toString();
+  }
+
   return "";
 }
